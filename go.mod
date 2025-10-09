0d88084fb2fe3a24c001d718661cabc224d3f580
Valentin Kaiser <valentin@kaiser-bielefeld.de>
2025-10-09 20:55:30 +0200

    [ADD] logging and version info to main entrypoint

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/valentin-kaiser/mingo
 
 go 1.24.5
 
-require github.com/valentin-kaiser/go-core v1.5.2
+require (
+	github.com/rs/zerolog v1.34.0
+	github.com/spf13/pflag v1.0.10
+	github.com/valentin-kaiser/go-core v1.5.2
+)
 
 require (
 	github.com/NYTimes/gziphandler v1.1.1 // indirect
@@ -13,8 +17,6 @@ require (
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/rs/zerolog v1.34.0 // indirect
-	github.com/spf13/pflag v1.0.10 // indirect
 	golang.org/x/crypto v0.35.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
--- main.go
+++ main.go
@@ -2,22 +2,61 @@ package main
 
 import (
 	"embed"
+	"fmt"
 
+	"github.com/rs/zerolog"
 	"github.com/valentin-kaiser/go-core/flag"
+	"github.com/valentin-kaiser/go-core/logging"
+	"github.com/valentin-kaiser/go-core/logging/log"
+	"github.com/valentin-kaiser/go-core/version"
 	"github.com/valentin-kaiser/go-core/web"
+	"github.com/valentin-kaiser/go-core/zlog"
 )
 
 var (
-	port uint16 = 8080
+	loglevel int    = 1
+	port     uint16 = 8080
 	//go:embed index.html
 	index embed.FS
 )
 
 func init() {
 	flag.Register("port", &port, "Port to listen on")
+	flag.Register("loglevel", &loglevel, "Log level (-1=trace, 0=debug, 1=info, 2=warn, 3=error, 4=fatal, 5=panic)")
+
+	flag.Init()
 }
 
 func main() {
+	zlog.Logger().WithConsole().WithLogFile().Init("mingo", zerolog.Level(loglevel))
+	logging.SetGlobalAdapter(logging.NewZerologAdapter().SetLevel(logging.Level(loglevel)))
+	logging.Debug(flag.Debug)
+
+	log.Info().Msgf("=== Mingo %s ===", version.String())
+	if flag.Debug {
+		log.Debug().Msg("running in debug mode")
+		log.Debug().Msgf("data path: %s", flag.Path)
+		log.Debug().Msgf("git tag: %s", version.GitTag)
+		log.Debug().Msgf("git commit: %s", version.GitCommit)
+		log.Debug().Msgf("git short: %s", version.GitShort)
+		log.Debug().Msgf("build date: %s", version.BuildDate)
+		log.Debug().Msgf("runtime version: %s %s", version.GoVersion, version.Platform)
+
+		for _, mod := range version.Modules {
+			log.Debug().Msgf("module => %s %s %s", mod.Path, mod.Version, mod.Sum)
+		}
+	}
+
+	if flag.Help {
+		flag.PrintHelp()
+		return
+	}
+
+	if flag.Version {
+		fmt.Print(version.String())
+		return
+	}
+
 	err := web.New().WithPort(port).WithCORSHeaders().WithGzip().WithLog().WithFS([]string{"/"}, index).Start().Error
 	if err != nil {
 		panic(err)
